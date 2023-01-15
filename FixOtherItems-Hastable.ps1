f6ff23822746740bb5c0ecfe49d85a7d4ba63a6c
DarrenGowing <122686407+DarrenGowing@users.noreply.github.com>
2023-01-15 15:23:34 +0000

    Hash table to create start menu items and desktop
    Added a script that adds on a hash table

---
+++ FixOtherItems-Hastable.ps1
@@ -0,0 +1,58 @@
+#Run def updates
+."C:\Program Files\Windows Defender\MpCmdRun.exe" -removedefinitions -dynamicsignatures
+."C:\Program Files\Windows Defender\MpCmdRun.exe" -SignatureUpdate
+
+#Create a Hashtable
+
+$listofprograms = @{
+
+    #value order: TestPath, TargetApp, Program Location, $description
+
+    "Key1" = @("C:\Program Files (x86)\Scomis\Hosted Applications\HostedAppsConnector.exe", "C:\Program Files (x86)\Scomis\Hosted Applications\BootStrap.exe", "$env:PROGRAMDATA\Microsoft\Windows\Start Menu\Programs\Scomis\Scomis Hosted Applications.lnk", "Scomis Hosted Applications")
+    "Key2" = @("Value2a", "Value2b", "Value2c", "Value2d")
+    "Key3" = @("Value3a", "Value3b", "Value3c", "Value3d")
+    "Key4" = @("Value4a", "Value4b", "Value4c", "Value4d")
+}
+
+#Loop though hash table
+
+$listofprograms.GetEnumerator() | ForEach-Object {
+    
+    $key = $_.Key
+    $TestPathVar = $_.Value[0]
+    $target = $_.Value[1]
+    $shortcut_path = $_.Value[2]
+    $description = $_.Value[3]
+
+    #Write-Host "$TestPathVar,$target,$shortcut_path,$description"
+
+    #Scomis
+    if ((Test-Path -Path "$TestPathVar")) {
+
+        #Start Menu
+
+        $workingdirectory = (Get-ChildItem $target).DirectoryName
+        $WshShell = New-Object -ComObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut($shortcut_path)
+        $Shortcut.TargetPath = $target
+        $Shortcut.Description = $description
+        $shortcut.WorkingDirectory = $workingdirectory
+        $Shortcut.Save()
+        Start-Sleep -Seconds 1
+
+        #Desktop
+
+        $workingdirectory = (Get-ChildItem $target).DirectoryName
+        $WshShell = New-Object -ComObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut($shortcut_path)
+        $Shortcut.TargetPath = $target
+        $Shortcut.Description = $description
+        $shortcut.WorkingDirectory = $workingdirectory
+        $Shortcut.Save()
+        Start-Sleep -Seconds 1
+
+    }
+    else {
+        Write-Host "No Scomis"
+    }
+}
